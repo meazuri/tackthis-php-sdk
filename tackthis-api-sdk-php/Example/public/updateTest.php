5be20a6d9bef6e3b1f5f4d24816d27eb0cb6696c
Seint San Thandar Bo <seintsan2009@gmail.com>
2014-08-15 14:54:20 +0800

    Add new Test Update

--- tackthis-api-sdk-php/Example/public/updateTest.php
+++ tackthis-api-sdk-php/Example/public/updateTest.php
@@ -4,4 +4,10 @@
  * User: seintsan
  * Date: 8/15/14
  * Time: 2:46 PM
- */ 
+ */
+$username = "developers@paywhere.com";
+$password = "developers@paywhere.com";
+
+$result = \Tackthis\Auth\Authentication::login($username,$password );
+$auth = \TackThis\Auth\Authentication::check_auth();
+var_dump($auth);
